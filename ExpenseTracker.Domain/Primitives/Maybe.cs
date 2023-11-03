74eea2da6bd22e225d6474f8a10a9e3adb99a4e7
SingleUnitBox <cezaryzukowski90@gmail.com>
2023-11-03 22:45:06 +0000

    add Maybe, Result

---
+++ ExpenseTracker.Domain/Primitives/Maybe.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ExpenseTracker.Domain.Primitives
+{
+    public struct Maybe<T> : IEquatable<Maybe<T>> where T : class
+    {
+        private readonly T _value;
+        public T Value
+        {
+            get
+            {
+                if (HasNoValue)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _value;
+            }
+        }
+        public bool HasValue => _value != null;
+        public bool HasNoValue => !HasValue;
+        public Maybe(T value)
+        {
+            _value = value;
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Maybe<T>))
+            {
+                return false;
+            }
+            var other = (Maybe<T>)obj;
+
+            return Equals(other);
+        }
+
+        public bool Equals(Maybe<T> other)
+        {
+            if (HasNoValue && other.HasNoValue)
+            {
+                return true;
+            }
+            if (HasNoValue || other.HasNoValue)
+            {
+                return false;
+            }
+            return _value.Equals(other._value);
+        }
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+        public static bool operator ==(Maybe<T> maybe, T value)
+        {
+            return maybe.HasValue && maybe.Value.Equals(value);
+        }
+        public static bool operator !=(Maybe<T> maybe, T value)
+        {
+            return !(maybe == value);
+        }
+        public static bool operator ==(Maybe<T> first, Maybe<T> second)
+        {
+            return first.Equals(second);
+        }
+        public static bool operator !=(Maybe<T> first, Maybe<T> second)
+        {
+            return !(first == second);
+        }
+        public static implicit operator Maybe<T>(T value)
+        {
+            return new Maybe<T>(value);
+        }
+        public T Unwrap()
+        {
+            return HasValue ? Value : default;
+        }
+
+        public TProperty Unwrap<TProperty>(Func<T, TProperty> selector)
+        {
+            return HasValue ? selector(Value) : default;
+        }
+    }
+}
---
+++ ExpenseTracker.Domain/Primitives/Result.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ExpenseTracker.Domain.Primitives
+{
+    public class Result
+    {
+        public bool Success { get; }
+        public bool Failure => !Success;
+        public string Error { get; }
+        protected Result(bool success, string error)
+        {
+            if (success && !string.IsNullOrWhiteSpace(error))
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (!success && string.IsNullOrWhiteSpace(error))
+            {
+                throw new InvalidOperationException();
+            }
+
+            Success = success;
+            Error = error;
+        }
+        public static Result Fail(string error) => new Result(false, error);
+        public static Result<TValue> Fail<TValue>(string error) => new Result<TValue>(default, false, error);
+        public static Result Ok() => new Result(true, string.Empty);
+        public static Result<TValue> Ok<TValue>(TValue value) => new Result<TValue>(value, true, string.Empty);
+        public static Result FirstFailureOrSuccess(params Result[] results)
+        {
+            foreach (Result result in results)
+            {
+                if (result.Failure)
+                {
+                    return result;
+                }               
+            }
+            return Ok();
+        }
+    }
+    public class Result<TValue> : Result
+    {
+        private readonly TValue _value;
+        public TValue Value
+        {
+            get
+            {
+                if (!Success)
+                {
+                    throw new InvalidOperationException();
+                }
+                return _value;
+            }
+        }
+        protected internal Result(TValue value, bool success, string error) : base (success, error)
+        {
+            _value = value;
+        }
+    }
+}
